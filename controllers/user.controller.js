306164d5066bd8aab1b912009ff4e486c794ddca
Giang Nguyen <giangvubinhn@vt.edu>
2023-02-27 17:41:46 -0500

    user login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,22 @@
 const userService = require('../services/user.service');
 
 async function register(req, res){
-
   const userRegisterRequestObject = req.body;
   const action = await userService.register(userRegisterRequestObject);
   return res.status(action.status).json(action.result);
 }
 
 async function login(req, res){
+  const userLoginRequestObject = req.body;
+  const action = await userService.login(userLoginRequestObject);
+  if(action.status !== 200){
+    return res.status(action.status).json(action.result)
+  }
+  return res.cookie("access_token", action.result.data, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  }).status(action.status).json(action.result)
+
 
 }
 
